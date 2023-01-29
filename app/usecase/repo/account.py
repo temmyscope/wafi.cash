ecebb3e6965570d05ed134dd63a53391f9b7740c
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2023-01-29 19:14:34 +0100

    Add User and Account Repo Handlers

--- app/usecase/repo/account.py
+++ app/usecase/repo/account.py
@@ -2,9 +2,42 @@ from app.config.config import USER_DATABASE
 
 def rp_deposit(email: str, amount: float) -> bool:
   print("\n-------------------------------------------------\n")
+  if email not in USER_DATABASE:
+    print("Current User Is Invalid")
+    return False
+  
+  print("funding your account...\n")
+  USER_DATABASE[email]["balance"] += amount 
+  print("Accound🏦 Funded Successfully")
+  return True
 
 def rp_send_money(email: str, amount: float, recipient: str) -> bool:
   print("\n-------------------------------------------------\n")
+  if email not in USER_DATABASE:
+    print("Current User Is Invalid")
+    return False
+  if recipient not in USER_DATABASE:
+    print("Recipient Does Not Exist")
+    return False
+  elif USER_DATABASE[email]["balance"] >= amount:
+    print("Sending...")
+    USER_DATABASE[email]["balance"] -= amount
+    USER_DATABASE[recipient]["balance"] += amount
+    print("{} Sent✅".format(amount))
+    return True
+  else:
+    print("Insufficient Fund🚫")
+    return False
 
 def rp_withdraw(email: str, amount: float) -> bool:
   print("\n-------------------------------------------------\n")
+  if email not in USER_DATABASE:
+    print("Current User Is Invalid")
+    return False
+  elif USER_DATABASE[email]["balance"] >= amount:
+    print("Withdrawing To Bank...")
+    USER_DATABASE[email]["balance"] -= amount
+    print("Withdrawal of {} USD Completed✅".format(amount))
+    return True
+
+
--- app/usecase/repo/user.py
+++ app/usecase/repo/user.py
@@ -1,7 +1,17 @@
 from app.config.config import USER_DATABASE
 
 def create_account(email: str, name: str) -> bool:
-  pass
+  print("\n-------------------------------------------------\n")
+  print("validating...\n")
+  if email in USER_DATABASE:
+    print("\n-------------------------------------------------\n")
+    print("An Account with that email already exists")
+    return False
+  else:
+    print("\n-------------------------------------------------\n")
+    USER_DATABASE[email] = {"balance": 0, "name": name, "email": email}
+    print("Account Created...\n")
+    return True
 
 def authenticate(email: str) -> bool:
   if email in USER_DATABASE:
