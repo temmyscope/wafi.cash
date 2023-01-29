d84e335251cae10acab4af7f14234c8b9e39e3ac
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2023-01-29 20:08:57 +0100

    Convert Emails To Lowercase

--- app/usecase/repo/user.py
+++ app/usecase/repo/user.py
@@ -3,18 +3,18 @@ from app.config.config import USER_DATABASE
 def create_account(email: str, name: str) -> bool:
   print("\n-------------------------------------------------\n")
   print("validating...\n")
-  if email in USER_DATABASE:
+  if email.lower() in USER_DATABASE:
     print("\n-------------------------------------------------\n")
     print("An Account with that email already exists")
     return False
   else:
     print("\n-------------------------------------------------\n")
-    USER_DATABASE[email] = {"balance": 0, "name": name}
+    USER_DATABASE[email.lower()] = {"balance": 0, "name": name}
     print("Account Created...\n")
     return True
 
 def authenticate(email: str) -> bool:
-  if email in USER_DATABASE:
+  if email.lower() in USER_DATABASE:
     return True
   else:
     return False
